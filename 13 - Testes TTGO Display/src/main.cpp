c8bfaf37677f04ccaacfc8daeb2a06e6c5a291da
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2022-12-21 22:47:33 -0300

    OK

--- 13 - Testes TTGO Display/src/main.cpp
+++ 13 - Testes TTGO Display/src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
+#include <TFT_eSPI.h>
+#include <SPI.h>
+
+// construtor
+TFT_eSPI tft=TFT_eSPI();
 
 void setup() {
-  // put your setup code here, to run once:
+  //inicializa a tela
+  tft.init();
+
+  //preenche a tela com uma cor
+  //a cor pode ser um código haxadecimal da cor
+  tft.fillScreen(TFT_BLUE);
+  delay(1000);
+  tft.fillScreen(0x05DC);
+
+  //Define a cor do texto e do fundo do texto
+  tft.setTextColor(TFT_RED, 0x05DC);
+
+  //Coloca a String na tela, nas posições X=30 e Y = 30.
+  //A origem é no canto superior esquerdo da tela
+  //O terceiro argumento é a fonte. Cada fonte tem seu tamanho
+  tft.drawString("Hello World", 30, 30,2);
+  
 }
 
+int count = 0;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  count++;
+  tft.drawString(String(count), 50, 150, 7); //A fonte 7 é somente para números. Usando a classe String pra converter número pra String
+  delay(1000);
 }
